aa40064bf3ec1fd08ff973fa1d18f47a07cff9e6
kazuki0924 <42589054+kazuki0924@users.noreply.github.com>
2020-04-19 02:39:46 +0900

    3

--- src/App.vue
+++ src/App.vue
@@ -6,10 +6,18 @@
       </v-card-title>
       <v-card-text>
         <v-form>
-          <v-text-field label="Username" />
-          <v-text-field type="password" label="Password" />
+          <v-text-field label="Username" prepend-icon="mdi-account-circle" />
+          <v-text-field
+            type="password"
+            label="Password"
+            prepend-icon="mdi-lock"
+          />
         </v-form>
       </v-card-text>
+      <v-card-actions>
+        <v-btn>Register</v-btn>
+        <v-btn>Login</v-btn>
+      </v-card-actions>
     </v-card>
   </v-app>
 </template>
