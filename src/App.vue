61f84c1ba4db23f0cd2e0d007dc2c6eaed9bc96c
kazuki0924 <42589054+kazuki0924@users.noreply.github.com>
2020-04-19 15:51:45 +0900

    12

--- src/App.vue
+++ src/App.vue
@@ -51,6 +51,10 @@ export default {
         {
           label: 'Login',
           url: '/login'
+        },
+        {
+          label: 'Dashboard',
+          url: '/dashboard'
         }
       ]
     }
--- src/router/index.js
+++ src/router/index.js
@@ -23,6 +23,11 @@ const routes = [
     path: '/login',
     name: 'login',
     component: () => import('../views/Login.vue')
+  },
+  {
+    path: '/dashboard',
+    name: 'dashboard',
+    component: () => import('../views/Dashboard.vue')
   }
 ]
 
