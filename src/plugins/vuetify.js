157412752b15172d62041cd5e26a3a2ec7e72026
kazuki0924 <42589054+kazuki0924@users.noreply.github.com>
2020-04-19 22:23:42 +0900

    28

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -4,7 +4,15 @@ import Vuetify from 'vuetify/lib'
 Vue.use(Vuetify)
 
 export default new Vuetify({
-	icons: {
-		iconfont: 'mdi'
-	}
+  icons: {
+    iconfont: 'mdi'
+  },
+  theme: {
+    light: {
+      primary: '#41B883'
+    },
+    dark: {
+      primary: '#34495E'
+    }
+  }
 })
