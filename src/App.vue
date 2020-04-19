63d95ed62381627f32be890193dfa1dd77fa037b
kazuki0924 <42589054+kazuki0924@users.noreply.github.com>
2020-04-19 14:41:09 +0900

    10

--- src/App.vue
+++ src/App.vue
@@ -3,8 +3,8 @@
     <v-app-bar app color="primary" dark>
       <v-toolbar-title>Vuetify Dashboard</v-toolbar-title>
       <v-spacer></v-spacer>
-      <v-btn text rounded>Home</v-btn>
-      <v-btn text rounded>Login</v-btn>
+      <v-btn text rounded to="/">Home</v-btn>
+      <v-btn text rounded to="/login">Login</v-btn>
     </v-app-bar>
     <v-content>
       <router-view></router-view>
